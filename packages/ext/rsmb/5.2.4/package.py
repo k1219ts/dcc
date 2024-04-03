0d796b6692b10a2449f6ceeb097ccd301d85ffa9
taeseob.kim <k1219ts@gmail.com>
2024-04-03 11:39:22 +0900

    rmb

---
+++ packages/ext/rsmb/5.2.4/package.py
@@ -0,0 +1,17 @@
+name = 'rsmb'
+
+def commands():
+    import os
+
+    appdir = '/opt/plugins/rsmb/{}'.format(version)
+    if not os.path.exists(appdir):
+        stop('Not installed ReelSmart MotionBlur 5.2.4.')
+
+    env.OFX_PLUGIN_PATH.append(appdir)
+    try:
+        if env.SITE == "KOR":
+            env.RVL_SERVER.set('10.10.10.108')
+        else:
+            env.RVL_SERVER.set('11.0.2.44')
+    except:
+        env.RVL_SERVER.set('10.10.10.108')
