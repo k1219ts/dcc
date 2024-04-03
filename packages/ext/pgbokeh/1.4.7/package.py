df0d0ea82f79e474896943c82d41aa40ddd37ac4
taeseob.kim <k1219ts@gmail.com>
2024-04-03 11:36:58 +0900

    pg

---
+++ packages/ext/pgbokeh/1.4.7/package.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+name = 'pgbokeh'
+
+def commands():
+    import os
+
+    nukeVer = getenv("REZ_NUKE_MAJOR_VERSION") + '.' + getenv("REZ_NUKE_MINOR_VERSION")
+    pluginDir = '/opt/plugins/bokeh/Bokeh-v{VERSION}_Nuke{NUKE_VER}-linux'.format(VERSION=version,
+                                                                                  NUKE_VER=nukeVer)
+    if not os.path.exists(pluginDir):
+        stop('Not installed pgBokeh-1.4.7 in Nuke-{NUKE_VER}'.format(NUKE_VER=nukeVer))
+
+    try:
+        if env.SITE == "KOR":
+            env.peregrinel_LICENSE.set('5053@10.10.10.109')
+        else:
+            env.peregrinel_LICENSE.set('5053@11.0.2.43')
+    except:
+        env.peregrinel_LICENSE.set('5053@10.10.10.109')
+
+    env.PG_BOKEH_ABORT_ON_LICENSE_FAIL.set(1) # if license error, abort error
+
+    env.NUKE_PATH.prepend(pluginDir)
